51da5164ccdfb4726327affbde08b1ea963e5a7b
Furkan dogu <furkandogu@Furkans-MacBook-Pro.local>
2021-05-05 23:03:43 +0200

    knex-config

--- knexfile.js
+++ knexfile.js
@@ -1,16 +1,20 @@
 require("./utils/env");
 
+const connection = process.env.DATABASE_URL
+  ? { connectionString: process.env.DATABASE_URL }
+  : {
+      host: process.env.DATABASE_HOST,
+      port: process.env.DATABASE_PORT
+        ? Number.parseInt(process.env.DATABASE_PORT, 10)
+        : 5432,
+      user: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+    };
+
 module.exports = {
   client: "pg",
-  connection: {
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT
-      ? Number.parseInt(process.env.DATABASE_PORT, 10)
-      : 5432,
-    user: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-  },
+  connection,
   migrations: {
     directory: "./db/migrations",
   },
