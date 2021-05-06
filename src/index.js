35462d01850567a4f21877da86eb7ed31ab3fc40
Furkan dogu <furkandogu@Furkans-MacBook-Pro.local>
2021-05-06 15:10:18 +0200

    return-response-with-timeout

--- src/index.js
+++ src/index.js
@@ -10,17 +10,22 @@ const knex = Knex(knexConfig);
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(async (_, res) => {
+  console.log("Request received");
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
 
   const users = await knex("users").select();
+  console.log("Data from db received");
 
-  res.end(
-    `Some random names;\n${users.reduce(
-      (str, user) => `${str}\n${user.name} ${user.surname}`,
-      "---------------------------------------------------"
-    )}`
-  );
+  setTimeout(() => {
+    console.log("Sending response after 35s");
+    res.end(
+      `Some random names;\n${users.reduce(
+        (str, user) => `${str}\n${user.name} ${user.surname}`,
+        "---------------------------------------------------"
+      )}`
+    );
+  }, 35000);
 });
 
 server.listen(port, () => {
